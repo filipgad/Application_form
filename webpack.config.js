87e294d39436ae74e377a3d74d974cb1e6039bde
Filip Godziński <godzinski.filip@gmail.com>
2018-02-05 19:52:29 +0100

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: ["./src/jsx/main.jsx","./src/scss/main.scss"],
+    output: { filename: "./dist/js/out.js" },
+    devServer: {
+        inline: true,
+        contentBase: './',
+        port: 3000
+    },
+    watch: true,
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: { presets: ['es2015', 'stage-2', 'react'] }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader?url=false', 'sass-loader']
+                })
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'url-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('./dist/css/style.css')
+    ]
+};
