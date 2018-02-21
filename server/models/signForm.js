cbe84e1e4f1ab728b2a80eef6a9ee8c9a905c17c
Filip Godziński <godzinski.filip@gmail.com>
2018-02-21 01:25:48 +0100

    error massage

--- server/models/signForm.js
+++ server/models/signForm.js
@@ -4,19 +4,19 @@ const Schema = mongoose.Schema;
 const SignFormSchema = new Schema({
     firstName: {
         type: String,
-        required: true
+        required: [true, 'Please enter your name']
     },
     lastName: {
         type: String,
-        required: true
+        required: [true, 'Please enter your last name']
     },
     email: {
         type: String,
-        required: true
+        required: [true, 'Please enter your email']
     },
     eventDate: {
         type: String,
-        required: true
+        required: [true, 'Please choose the event date']
     }
 });
 
